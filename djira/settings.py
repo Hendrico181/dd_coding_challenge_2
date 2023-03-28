ad0cfadae29441811859424a0b36739cafd768ba
Hendrico181 <shendrico@gmail.com>
2023-03-28 07:17:18 +0200

    Update settings.py

--- djira/settings.py
+++ djira/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,7 +124,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'djira', 'static')
 ]
 
-django_heroku.setting(locals())
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
